af04f3e5c8b3b6328d5123cbab54201c46862aea
Bartek thindil Jasicki <thindil@laeran.pl>
2019-05-03 10:27:35 +0200

    added documentation for types

--- roboada.py
+++ roboada.py
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# FIXME: Can't detect end of internal subprograms with another subprograms inside.
+
 import glob, os, sys
 
 if not os.path.exists('result'):
@@ -47,7 +49,7 @@ for filename in glob.glob(filenames):
             else:
                 packagename = line.split(" ")[2]
         doclines = []
-        if not line.startswith("--") and (line.startswith("procedure") or line.startswith("function") or line.find(": ") > 0) and packagename != "":
+        if not line.startswith("--") and (line.startswith("procedure") or line.startswith("function") or line.find(": ") > 0 or line.startswith("type") or line.startswith("subtype") or (line.startswith("package") and line.split(" ")[1] != packagename and line.find("is new") > 0)) and packagename != "":
             # parse function and procedures
             functionname = ""
             if line.startswith("procedure") or line.startswith("function"):
@@ -58,6 +60,11 @@ for filename in glob.glob(filenames):
                     doclines.append("-- ****f* " + packagename + "/" + functionname + "\n");
                 else:
                     doclines.append("-- ****if* " + packagename + "/" + functionname + "\n");
+            elif line.startswith("type") or line.startswith("subtype") or line.startswith("package"):
+                if extension == ".ads":
+                    doclines.append("-- ****t* " + packagename + "/" + line.split(" ")[1] + "\n");
+                else:
+                    doclines.append("-- ****it* " + packagename + "/" + line.split(" ")[1] + "\n");
             else:
                 if extension == ".ads":
                     doclines.append("-- ****v* " + packagename + "/" + line[:line.find(":")] + "\n");
@@ -98,6 +105,18 @@ for filename in glob.glob(filenames):
                 if len(content) > i + 1:
                     while content[i + 1].strip().startswith("pragma"):
                         i += 1
+            elif line.startswith("type") or line.startswith("subtype") or line.startswith("package"):
+                if line.find("record") > -1 or content[i + 1].find("record") > -1:
+                    while content[i].find("end record;") == -1:
+                        i += 1
+                else:
+                    bracketsopen = content[i].count("(") - content[i].count(")")
+                    while bracketsopen > 0:
+                        i += 1
+                        bracketsopen += (content[i].count("(") - content[i].count(")"))
+                    if content[i].find("record") > -1 or content[i + 1].find("record") > -1:
+                        while content[i].find("end record;") == -1:
+                            i += 1
             else:
                 while content[i].find(";") == -1:
                     i += 1
@@ -122,7 +141,7 @@ for filename in glob.glob(filenames):
             i += 2
             if len(content) > i:
                 line = content[i].strip()
-                if not line.startswith("--") and (line.startswith("procedure") or line.startswith("function") or line.find(":") > 0):
+                if not line.startswith("--") and (line.startswith("procedure") or line.startswith("function") or line.find(":") > 0 or line.startswith("package")):
                     i -= 1
         i += 1
     # save data to new file
